e7f843f0f297ae223f187736405a9b8df996a0e9
MANAna7 <fragileaanm@gmail.com>
2024-11-22 21:06:39 +0900

    true first commit

---
+++ src/main/java/net/mana7na/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.mana7na.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
