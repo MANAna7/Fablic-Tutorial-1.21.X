6503c6093be9b692a8af9defbdd857dda7c7cc4b
MANAna7 <fragileaanm@gmail.com>
2024-12-10 01:54:33 +0900

    true ModBlockTagProvider made commit

--- src/main/java/net/mana7na/tutorialmod/TutorialModDataGenerator.java
+++ src/main/java/net/mana7na/tutorialmod/TutorialModDataGenerator.java
@@ -2,10 +2,13 @@ package net.mana7na.tutorialmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.mana7na.tutorialmod.datagen.ModBlockTagProvider;
 
 public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
 	}
 }
---
+++ src/main/java/net/mana7na/tutorialmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,4 @@
+package net.mana7na.tutorialmod.datagen;
+
+public class ModItemTagProvider {
+}
