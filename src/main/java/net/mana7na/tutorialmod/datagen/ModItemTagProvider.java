88cb3a255aa8fc586a65bb156f3be8e557713574
MANAna7 <fragileaanm@gmail.com>
2024-12-12 15:26:46 +0900

    ModLootTableProvider made commit for note pc

--- src/main/java/net/mana7na/tutorialmod/datagen/ModItemTagProvider.java
+++ src/main/java/net/mana7na/tutorialmod/datagen/ModItemTagProvider.java
@@ -1,4 +1,28 @@
 package net.mana7na.tutorialmod.datagen;
 
-public class ModItemTagProvider {
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.mana7na.tutorialmod.item.ModItems;
+import net.mana7na.tutorialmod.util.ModTags;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryWrapper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture, @Nullable BlockTagProvider blockTagProvider) {
+        super(output, completableFuture, blockTagProvider);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.PINK_GARNET)
+                .add(ModItems.RAW_PINK_GARNET)
+                .add(Items.COAL)
+                .add(Items.STICK)
+                .add(Items.APPLE);
+    }
 }
