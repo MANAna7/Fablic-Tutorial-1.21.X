9e14bb225a4660d9dcd40fcd0e2a048b24da68fc
MANAna7 <fragileaanm@gmail.com>
2025-01-22 02:18:46 +0900

    ModToolMaterials made commit

--- src/main/java/net/mana7na/tutorialmod/util/ModTags.java
+++ src/main/java/net/mana7na/tutorialmod/util/ModTags.java
@@ -1,17 +1,19 @@
 package net.mana7na.tutorialmod.util;
 
 import net.mana7na.tutorialmod.TutorialMod;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class ModTags {
     public static class Blocks {
+        public static final TagKey<Block> NEED_PINK_GARNET_TOOL = createTag("needs_pink_garnet_tool");
+        public static final TagKey<Block> INCORRECT_FOR_PINK_GARNET_TOOL = createTag("incorrect_for_pink_garnet_tool");
 
-        private static TagKey<Item> createTag(String name){
-            return TagKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID,name));
+        private static TagKey<Block> createTag(String name){
+            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(TutorialMod.MOD_ID,name));
         }
     }
 
