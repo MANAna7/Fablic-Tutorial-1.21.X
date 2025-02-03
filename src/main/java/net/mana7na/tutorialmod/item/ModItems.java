337775eb3e363763778f17bad1cd8b393c833800
MANAna7 <fragileaanm@gmail.com>
2025-02-03 15:27:15 +0900

    HammerUsageEvent commit for note pc

--- src/main/java/net/mana7na/tutorialmod/item/ModItems.java
+++ src/main/java/net/mana7na/tutorialmod/item/ModItems.java
@@ -3,6 +3,7 @@ package net.mana7na.tutorialmod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.mana7na.tutorialmod.TutorialMod;
 import net.mana7na.tutorialmod.item.custom.ChiselItem;
+import net.mana7na.tutorialmod.item.custom.HammerItem;
 import net.minecraft.item.*;
 import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
@@ -47,6 +48,10 @@ public class ModItems {
     public static final Item PINK_GARNET_HOE = registerItem("pink_garnet_hoe",
             new HoeItem(ModToolMaterials.PINK_GARNET,new Item.Settings()
                     .attributeModifiers(HoeItem.createAttributeModifiers(ModToolMaterials.PINK_GARNET,0,-3.0f))));
+
+    public static final Item PINK_GARNET_HAMMER = registerItem("pink_garnet_hammer",
+            new HammerItem(ModToolMaterials.PINK_GARNET,new Item.Settings()
+                    .attributeModifiers(PickaxeItem.createAttributeModifiers(ModToolMaterials.PINK_GARNET,7,-3.8f))));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID,name), item);
